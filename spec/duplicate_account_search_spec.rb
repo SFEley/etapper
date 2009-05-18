516c91e1d337aad2ab580bd609caa87b65d45059
Stephen Eley <sfeley@gmail.com>
2009-05-18 15:08:40 -0400

    Duplicate Account Search spec

---
+++ spec/duplicate_account_search_spec.rb
@@ -0,0 +1,47 @@
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+
+describe "Duplicate Account Search" do
+  it "allows a name" do
+    d = Etapper::DuplicateAccountSearch.new(:name => "John Smith")
+    d.name.should == "John Smith"
+  end
+  
+  it "allows a phone number" do
+    d = Etapper::DuplicateAccountSearch.new(:phone => "404-727-7972")
+    d.phoneNumber.should == "404-727-7972"
+  end
+  
+  it "allows an address" do
+    d = Etapper::DuplicateAccountSearch.new(:address => "825 Houston Mill Rd NE Suite 300")
+    d.address.should == "825 Houston Mill Rd NE Suite 300"
+  end
+  
+  it "allows an email address" do
+    d = Etapper::DuplicateAccountSearch.new(:email => "seley@aarweb.org")
+    d.email.should == "seley@aarweb.org"
+  end
+  
+  it "raises an exception if none of the above are provided" do
+    lambda {d = Etapper::DuplicateAccountSearch.new}.should raise_error(Etapper::BadValueError)
+  end
+  
+  it "sets allowEmailOnlyMatch to true if an email is provided" do
+    d = Etapper::DuplicateAccountSearch.new(:email => "seley@aarweb.org", :name => "Steve Eley")
+    d.allowEmailOnlyMatch.should be_true
+  end
+  
+  it "sets allowEmailOnlyMatch to false if an email is not provided" do
+    d = Etapper::DuplicateAccountSearch.new(:name => "Steve Eley", :phone => "404-727-7972")
+    d.allowEmailOnlyMatch.should be_false
+  end
+  
+  it "allows setting accountRoleTypes" do
+    d = Etapper::DuplicateAccountSearch.new(:email => "seley@aarweb.org", :account_role_types => :tributes_only)
+    d.accountRoleTypes.should == 2    
+  end
+
+  it "sets accountRoleTypes to donors only by default" do
+    d = Etapper::DuplicateAccountSearch.new(:phone => "404-727-7972")
+    d.accountRoleTypes.should == 1
+  end
+end
